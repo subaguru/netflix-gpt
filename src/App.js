bd4f7e7fb0c2e1b949ad425b097873a979255675
subaguru <subaguruprabakaran@gmail.com>
2024-10-21 22:04:38 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './index'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-800">
+      Welcome to my Project
+    </div>
+  );
+}
+
+export default App;
